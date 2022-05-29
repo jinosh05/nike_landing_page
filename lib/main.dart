985169641cb2e49b8c4dc121d83656110979a52c
jinoshprabhuraj5 <jinoshprabhuraj5@gmail.com>
2022-05-29 18:48:03 +0530

    Global Screen Breakpoints was Set

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,7 @@
-import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: const Text('Hello World'),
-          ),
-        ),
-      ),
-    );
-  }
+void main(List<String> args) {
+  ResponsiveSizingConfig.instance.setCustomBreakpoints(
+    const ScreenBreakpoints(desktop: 780, tablet: 480, watch: 200),
+  );
 }
