961dbca36a605c98ff44ed45f88f445b5d3952d0
jinoshprabhuraj5 <jinoshprabhuraj5@gmail.com>
2022-05-30 13:16:51 +0530

    Tab UI was completed

--- lib/presentations/landing_page/landing_tablet.dart
+++ lib/presentations/landing_page/landing_tablet.dart
@@ -33,61 +33,84 @@ class _LandingTabletState extends State<LandingTablet> {
               child: Stack(
             children: [
               Positioned(
-                  right: SizeConfig.width_40,
-                  top: SizeConfig.height_3,
-                  child: BlurCircleWidget(
-                    size: SizeConfig.imgSize_25,
-                  )),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    AppBase.shoeName,
-                    style: GoogleFonts.roboto(
-                      color: Colors.white,
-                      fontSize: SizeConfig.fs_11,
-                      fontWeight: FontWeight.w900,
-                    ),
-                  ),
-                  Text(
-                    AppBase.shoePrice,
-                    style: GoogleFonts.roboto(
-                      color: Colors.white,
-                      fontSize: SizeConfig.fs_03,
-                      fontWeight: FontWeight.w900,
-                    ),
-                  ),
-                  SizedBox(
-                    height: SizeConfig.height_1_5,
-                  ),
-                  Container(
-                    width: SizeConfig.width_70,
-                    child: Text(
-                      AppBase.shoeDesc,
-                      style: GoogleFonts.roboto(
-                        color: AppBase.descTextColor,
-                        fontSize: SizeConfig.fs_1_7,
-                        fontWeight: FontWeight.w600,
-                        height: 1.3,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: SizeConfig.height_2_5,
-                  ),
-                  GradiantButton(
-                    text: AppBase.addToCart,
-                    onPressed: () {
-                      debugPrint('Pressed Add to Cart');
-                    },
-                  )
-                ],
+                right: SizeConfig.width_35,
+                top: SizeConfig.height_3,
+                child: BlurCircleWidget(
+                  size: SizeConfig.imgSize_25 + SizeConfig.imgSize_2_5,
+                ),
               ),
-              Row()
+              detailsColumn(),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Image.asset(
+                  'assets/shoe.png',
+                  height: SizeConfig.imgSize_80,
+                ),
+              ),
+              Align(
+                  alignment: Alignment.centerRight,
+                  child: BlurCircleWidget(
+                    size: SizeConfig.imgSize_20 + SizeConfig.imgSize_2_5,
+                  )),
+              Positioned(
+                right: SizeConfig.width_40,
+                bottom: SizeConfig.height_3,
+                child: BlurCircleWidget(
+                  size: SizeConfig.imgSize_20,
+                ),
+              ),
             ],
           )),
         ],
       ),
+    );
+  }
+
+  Column detailsColumn() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          AppBase.shoeName,
+          style: GoogleFonts.roboto(
+            color: Colors.white,
+            fontSize: SizeConfig.fs_11,
+            fontWeight: FontWeight.w900,
+          ),
+        ),
+        Text(
+          AppBase.shoePrice,
+          style: GoogleFonts.roboto(
+            color: Colors.white,
+            fontSize: SizeConfig.fs_03,
+            fontWeight: FontWeight.w900,
+          ),
+        ),
+        SizedBox(
+          height: SizeConfig.height_1_5,
+        ),
+        Container(
+          width: SizeConfig.width_70,
+          child: Text(
+            AppBase.shoeDesc,
+            style: GoogleFonts.roboto(
+              color: AppBase.descTextColor,
+              fontSize: SizeConfig.fs_1_7,
+              fontWeight: FontWeight.w600,
+              height: 1.3,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: SizeConfig.height_2_5,
+        ),
+        GradiantButton(
+          text: AppBase.addToCart,
+          onPressed: () {
+            debugPrint('Pressed Add to Cart');
+          },
+        )
+      ],
     );
   }
 
