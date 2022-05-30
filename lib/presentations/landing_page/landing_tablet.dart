af68784f481877b7db20ff243301ed27710bc018
jinoshprabhuraj5 <jinoshprabhuraj5@gmail.com>
2022-05-30 10:17:12 +0530

    AppBar implemented Tab mode

--- lib/presentations/landing_page/landing_tablet.dart
+++ lib/presentations/landing_page/landing_tablet.dart
@@ -1,12 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:nike_landing_page/utils/size_config.dart';
 
-class LandingTablet extends StatelessWidget {
+import '../../env.dart';
+
+class LandingTablet extends StatefulWidget {
   const LandingTablet({Key? key}) : super(key: key);
 
   @override
+  State<LandingTablet> createState() => _LandingTabletState();
+}
+
+class _LandingTabletState extends State<LandingTablet> {
+  List<bool> _hover = [false, false, false, false];
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.transparent,
+    return Column(
+      children: [
+        SizedBox(
+          height: SizeConfig.height_1_5,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            SizedBox(),
+            for (var i = 0; i < AppBase.headings.length; i++)
+              InkWell(
+                onTap: () {
+                  debugPrint('Tapped');
+                },
+                onHover: (bool value) {
+                  setState(() {
+                    _hover[i] = value;
+                  });
+                },
+                child: Text(
+                  AppBase.headings[i],
+                  style: TextStyle(
+                    color: _hover[i] ? Colors.pink : Colors.white,
+                    fontSize: SizeConfig.fs_1_2,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            SizedBox(),
+          ],
+        )
+      ],
     );
   }
 }
